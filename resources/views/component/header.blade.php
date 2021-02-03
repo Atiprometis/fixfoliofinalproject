dacfeab55fa11c0cff29aa83b7d296012da64f48
Fang <fang.live-@msn.com>
2021-02-03 19:03:44 +0700

    add header dashboard

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -48,11 +48,15 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a href="/backoffice" class="dropdown-item">{{ __('Backoffice') }}</a>
+                            <a class="dropdown-item" href="/dashbord">
+                                Dashboard
+                            </a>
+                            {{-- <a href="/backoffice" class="dropdown-item">{{ __('Backoffice') }}</a> --}}
 
                             <a class="dropdown-item" href="/addid">
                                 portfolio
                             </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
