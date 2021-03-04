05920aa842e624bb1442c3aa16683a87d2a5765a
Fang <fang.live-@msn.com>
2021-03-04 18:14:15 +0700

    แก้ไข รูปตามคอร์ส

---
+++ resources/views/course/component/img.blade.php
@@ -0,0 +1,22 @@
+
+@if ($courseAll->course_category == 'อาหาร')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-0.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'ตักเย็บ')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-1.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'งานช่าง')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-2.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'เสริมสวย')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-3.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'ภาษาต่างประเทศ')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-4.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'นวด')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-5.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
+@if ($courseAll->course_category == 'งานฝีมือ')
+    <img class=" img-fluid col-12 p-0" src="{{ asset('access/images/banner/category-6.jpg') }}" alt="Responsive image" style=" height: 50vh;">
+@endif
